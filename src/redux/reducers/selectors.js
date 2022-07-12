b61cbf983b0197f0690a0e4da9af11bed364574f
hryhori <108329990+hryhori@users.noreply.github.com>
2022-07-12 16:24:32 +0300

    Added reselect(CreateSelector())

--- src/redux/reducers/selectors.js
+++ src/redux/reducers/selectors.js
@@ -1,5 +1,11 @@
+import { createSelector } from "reselect";
+
 export const getProfileSelector = (state) =>(state.ProfilePage.profile);
 
 export const getSocialLinksSelector = (state) => (state.ProfilePage.social);
+
+export const getLinks = createSelector(getSocialLinksSelector, (links)=>{
+    links.filter(l=>true);
+})
 
 export const getMyIdSelector = (state) => (state.auth.id);
